04cdb3ccd9f03aea8d85404336291e96db681bb0
harryhepburn <rafizansamian@gmail.com>
2024-07-20 16:38:11 +0800

    Update GuanoV16.py

--- GuanoV16.py
+++ GuanoV16.py
@@ -117,5 +117,10 @@ def main():
     st.write("---")
     st.success("Terima Kasih Kerana Menggunakan GUANO")
 
+    st.write("""
+    Dibangunkan oleh Team KIK Wilayah Raja Alias: **GUANO**
+    RAhandal
+    SEGALANYA FELDA
+    """)
 if __name__ == "__main__":
     main()
