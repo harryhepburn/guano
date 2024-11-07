d0e5d331eb45bb203df6ca1c1093477d0cb657f1
rfzn <rafizansamian@gmail.com>
2024-11-07 14:47:11 +0800

    Update GuanoV16.py

--- GuanoV16.py
+++ GuanoV16.py
@@ -165,14 +165,14 @@ def main():
     st.write(df)
     st.write('Nota: Andaian pengurangan hasil sehingga 30% setahun jika dibiar tanpa kawalan dan 10% jika dikawal.')
 
-    fig, ax = plt.subplots(figsize=(5, 3))
-    ax.plot(years, dirawat_yields, label='Kawalan', color='green', marker='x')
-    ax.plot(years, dibiar_yields, label='Tiada Kawalan',color='red', marker='o')
-    ax.set_xlabel('Tahun')
-    ax.set_ylabel('Hasil (MT)')
-    ax.set_title('Perbandingan Hasil Antara Kawalan dan Tiada Kawalan')
-    ax.legend()
-    ax.grid(True)
+    #fig, ax = plt.subplots(figsize=(5, 3))
+    #ax.plot(years, dirawat_yields, label='Kawalan', color='green', marker='x')
+    #ax.plot(years, dibiar_yields, label='Tiada Kawalan',color='red', marker='o')
+    #ax.set_xlabel('Tahun')
+    #ax.set_ylabel('Hasil (MT)')
+    #ax.set_title('Perbandingan Hasil Antara Kawalan dan Tiada Kawalan')
+    #ax.legend()
+    #ax.grid(True)
     
     st.pyplot(fig)
 
